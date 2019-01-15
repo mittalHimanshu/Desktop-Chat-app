f538695e40d384784b58ba1af9f5ad9d5ec6709a
Himanshu Mittal <mittalhimanshu121@gmail.com>
2019-01-15 19:11:09 +0530

    send message complete

--- server/SocketManager/index.js
+++ server/SocketManager/index.js
@@ -2,4 +2,8 @@ const io = require('../server')
 
 module.exports = socket => {
     console.log(socket.id)
+
+    socket.on('new-message', message => {
+        io.emit('new-message', message)
+    })
 }
